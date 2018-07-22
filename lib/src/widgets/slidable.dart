7866f60edf93d472bbbd7fb3cf94a8e3cc06b9ce
Romain Rastel <rromain35@gmail.com>
2018-07-22 12:14:39 +0200

    Fix animation type for SlidableActionBuilder

--- lib/src/widgets/slidable.dart
+++ lib/src/widgets/slidable.dart
@@ -4,38 +4,38 @@ import 'package:flutter/widgets.dart';
 const double _kActionsExtentRatio = 0.25;
 const double _kFastThreshold = 2500.0;
 
-typedef Widget SlidableActionBuilder(BuildContext context, int index, Animation<Offset> animation);
+typedef Widget SlidableActionBuilder(BuildContext context, int index, Animation<double> animation);
 
-abstract class SlidableActionDelegate{
+abstract class SlidableActionDelegate {
   const SlidableActionDelegate();
 
   /// Returns the child with the given index.
   ///
   /// Must not return null.
-  Widget build(BuildContext context, int index, Animation<Offset> animation);
+  Widget build(BuildContext context, int index, Animation<double> animation);
 
   /// Returns the number of actions this delegate will build.
   int get actionCount;
 }
 
-class SlidableActionBuilderDelegate extends SlidableActionDelegate{
+class SlidableActionBuilderDelegate extends SlidableActionDelegate {
   const SlidableActionBuilderDelegate({
-    this.builder,
-    this.actionCount,
-}) ;
+    @required this.builder,
+    @required this.actionCount,
+  }) : assert(actionCount != null && actionCount >= 0);
 
   final SlidableActionBuilder builder;
 
   final int actionCount;
 
   @override
-  Widget build(BuildContext context, int index, Animation<Offset> animation) => builder(context, index, animation);
+  Widget build(BuildContext context, int index, Animation<double> animation) => builder(context, index, animation);
 }
 
-class SlidableActionListDelegate extends SlidableActionDelegate{
+class SlidableActionListDelegate extends SlidableActionDelegate {
   const SlidableActionListDelegate({
-    this.actions,
-  }) : super();
+    @required this.actions,
+  });
 
   final List<Widget> actions;
 
@@ -43,7 +43,7 @@ class SlidableActionListDelegate extends SlidableActionDelegate{
   int get actionCount => actions?.length ?? 0;
 
   @override
-  Widget build(BuildContext context, int index, Animation<Offset> animation) => actions[index];
+  Widget build(BuildContext context, int index, Animation<double> animation) => actions[index];
 }
 
 /// A handle to various properties useful while calling [SlidableDelegate.buildActions].
@@ -63,13 +63,11 @@ class SlidableDelegateContext {
   final Slidable slidable;
 
   /// The current actions that have to be shown.
-  SlidableActionDelegate get actionDelegate =>
-      showLeftActions ? slidable.leftActionDelegate : slidable.rightActionDelegate;  
-  
+  SlidableActionDelegate get actionDelegate => showLeftActions ? slidable.leftActionDelegate : slidable.rightActionDelegate;
+
   int get actionCount => actionDelegate?.actionCount ?? 0;
 
-  double get totalActionsWidth =>
-      slidable.actionExtentRatio * (actionCount);
+  double get totalActionsWidth => slidable.actionExtentRatio * (actionCount);
 
   /// Whether the left actions have to be shown.
   final bool showLeftActions;
@@ -80,8 +78,8 @@ class SlidableDelegateContext {
   final AnimationController controller;
 
   /// Builds the slide actions using the active [SlidableActionDelegate]'s builder.
-  List<Widget> buildActions(BuildContext context, Animation<Offset> animation){
-    return List.generate(actionCount, (int index) => actionDelegate.build(context, index, animation));
+  List<Widget> buildActions(BuildContext context) {
+    return List.generate(actionCount, (int index) => actionDelegate.build(context, index, controller.view));
   }
 }
 
@@ -104,8 +102,7 @@ abstract class SlidableDelegate {
   const SlidableDelegate({
     double fastThreshold,
   })  : fastThreshold = fastThreshold ?? _kFastThreshold,
-        assert(fastThreshold == null || fastThreshold >= .0,
-            'fastThreshold must be positive');
+        assert(fastThreshold == null || fastThreshold >= .0, 'fastThreshold must be positive');
 
   /// The threshold used to know if a movement was fast and request to open/close the actions.
   final double fastThreshold;
@@ -177,8 +174,7 @@ class SlidableStrechDelegate extends SlidableStackDelegate {
                     bottom: .0,
                     width: constraints.maxWidth * animation.value.dx.abs(),
                     child: new Row(
-                      children:                          
-                          ctx.buildActions(context, animation).map((a) => Expanded(child: a)).toList(),
+                      children: ctx.buildActions(context).map((a) => Expanded(child: a)).toList(),
                     ),
                   )
                 ],
@@ -210,7 +206,7 @@ class SlidableBehindDelegate extends SlidableStackDelegate {
               bottom: .0,
               width: constraints.maxWidth * ctx.totalActionsWidth,
               child: new Row(
-                children: ctx.buildActions(context, null).map((a) => Expanded(child: a)).toList(),
+                children: ctx.buildActions(context).map((a) => Expanded(child: a)).toList(),
               ),
             )
           ],
@@ -251,8 +247,7 @@ class SlidableScrollDelegate extends SlidableStackDelegate {
                     bottom: .0,
                     width: totalWidth,
                     child: new Row(
-                      children:
-                          ctx.buildActions(context, animation).map((a) => Expanded(child: a)).toList(),
+                      children: ctx.buildActions(context).map((a) => Expanded(child: a)).toList(),
                     ),
                   )
                 ],
@@ -290,20 +285,16 @@ class SlidableDrawerDelegate extends SlidableStackDelegate {
             animation: ctx.controller,
             builder: (context, child) {
               return new Stack(
-                children: List.generate(ctx.actionCount, (index){
+                children: List.generate(ctx.actionCount, (index) {
                   // For the left items we have to reverse the order if we want the last item at the bottom of the stack.
                   int displayIndex = ctx.showLeftActions ? ctx.actionCount - index - 1 : index;
                   return new Positioned(
-                    left: ctx.showLeftActions
-                        ? animations[index].value.dx
-                        : null,
-                    right: ctx.showLeftActions
-                        ? null
-                        : animations[index].value.dx,
+                    left: ctx.showLeftActions ? animations[index].value.dx : null,
+                    right: ctx.showLeftActions ? null : animations[index].value.dx,
                     top: .0,
                     bottom: .0,
                     width: actionWidth,
-                    child: ctx.actionDelegate.build(context, displayIndex, animations[index]),
+                    child: ctx.actionDelegate.build(context, displayIndex, ctx.controller.view),
                   );
                 }),
               );
@@ -329,16 +320,15 @@ class Slidable extends StatefulWidget {
     double showAllActionsThreshold = 0.5,
     double actionExtentRatio = _kActionsExtentRatio,
     Duration movementDuration: const Duration(milliseconds: 200),
-  })  : this.builder(
-    key : key,
-      child : child,
-      delegate : delegate,
-      leftActionDelegate : new SlidableActionListDelegate(actions:  leftActions),
-    rightActionDelegate : new SlidableActionListDelegate(actions:  rightActions),
-    showAllActionsThreshold : showAllActionsThreshold,
-    actionExtentRatio : actionExtentRatio,
-    movementDuration  : movementDuration
-  );
+  }) : this.builder(
+            key: key,
+            child: child,
+            delegate: delegate,
+            leftActionDelegate: new SlidableActionListDelegate(actions: leftActions),
+            rightActionDelegate: new SlidableActionListDelegate(actions: rightActions),
+            showAllActionsThreshold: showAllActionsThreshold,
+            actionExtentRatio: actionExtentRatio,
+            movementDuration: movementDuration);
 
   Slidable.builder({
     Key key,
@@ -350,16 +340,9 @@ class Slidable extends StatefulWidget {
     this.actionExtentRatio = _kActionsExtentRatio,
     this.movementDuration: const Duration(milliseconds: 200),
   })  : assert(delegate != null),
-        assert(
-        showAllActionsThreshold != null &&
-            showAllActionsThreshold >= .0 &&
-            showAllActionsThreshold <= 1.0,
-        'showAllActionsThreshold must be between 0.0 and 1.0'),
-        assert(
-        actionExtentRatio != null &&
-            actionExtentRatio >= .0 &&
-            actionExtentRatio <= 1.0,
-        'actionExtentRatio must be between 0.0 and 1.0'),
+        assert(showAllActionsThreshold != null && showAllActionsThreshold >= .0 && showAllActionsThreshold <= 1.0,
+            'showAllActionsThreshold must be between 0.0 and 1.0'),
+        assert(actionExtentRatio != null && actionExtentRatio >= .0 && actionExtentRatio <= 1.0, 'actionExtentRatio must be between 0.0 and 1.0'),
         super(key: key);
 
   /// The widget below this widget in the tree.
@@ -388,19 +371,19 @@ class Slidable extends StatefulWidget {
   SlidableState createState() => SlidableState();
 }
 
-class SlidableState extends State<Slidable>
-    with TickerProviderStateMixin, AutomaticKeepAliveClientMixin<Slidable> {
+class SlidableState extends State<Slidable> with TickerProviderStateMixin, AutomaticKeepAliveClientMixin<Slidable> {
   @override
   void initState() {
     super.initState();
-    _controller =
-        new AnimationController(duration: widget.movementDuration, vsync: this)
-          ..addStatusListener(_handleShowAllActionsStatusChanged);
+    _controller = new AnimationController(duration: widget.movementDuration, vsync: this)..addStatusListener(_handleShowAllActionsStatusChanged);
   }
 
   void _handleShowAllActionsStatusChanged(AnimationStatus status) {
     if (status == AnimationStatus.completed && !_dragUnderway && !_opening) {
       _dragExtent = .0;
+    }
+
+    if(status == AnimationStatus.completed){
       setState(() {});
     }
   }
@@ -415,11 +398,9 @@ class SlidableState extends State<Slidable>
   }
 
   /// The current actions that have to be shown.
-  SlidableActionDelegate get actionDelegate =>
-      _showLeftActions ? widget.leftActionDelegate : widget.rightActionDelegate;
+  SlidableActionDelegate get actionDelegate => _showLeftActions ? widget.leftActionDelegate : widget.rightActionDelegate;
 
-  double get _overallDragAxisExtent =>
-      context.size.width * widget.actionExtentRatio * (actionDelegate?.actionCount ?? 0);
+  double get _overallDragAxisExtent => context.size.width * widget.actionExtentRatio * (actionDelegate?.actionCount ?? 0);
 
   @override
   void dispose() {
@@ -451,8 +432,7 @@ class SlidableState extends State<Slidable>
     if (!open && fast) {
       _opening = false;
       _controller.animateTo(0.0);
-    } else if (_controller.value >= widget.showAllActionsThreshold ||
-        (open && fast)) {
+    } else if (_controller.value >= widget.showAllActionsThreshold || (open && fast)) {
       _opening = true;
       _controller.animateTo(1.0);
     } else {
@@ -465,7 +445,8 @@ class SlidableState extends State<Slidable>
   Widget build(BuildContext context) {
     super.build(context); // See AutomaticKeepAliveClientMixin.
 
-    if ((widget.leftActionDelegate == null || widget.leftActionDelegate.actionCount == 0) && (widget.rightActionDelegate == null || widget.rightActionDelegate.actionCount == 0)) {
+    if ((widget.leftActionDelegate == null || widget.leftActionDelegate.actionCount == 0) &&
+        (widget.rightActionDelegate == null || widget.rightActionDelegate.actionCount == 0)) {
       return widget.child;
     }
 

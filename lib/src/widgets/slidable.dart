5119320a6e28f8106f2056e34b21d71ee71e0e07
brandon.maness <brandon.maness@amerch.com>
2019-05-16 07:05:14 -0500

    Fix NPE on widget.dismissal

--- lib/src/widgets/slidable.dart
+++ lib/src/widgets/slidable.dart
@@ -558,8 +558,13 @@ class SlidableState extends State<Slidable>
 
   double get _totalActionsExtent => widget.actionExtentRatio * (_actionCount);
 
-  double get _dismissThreshold =>
-      widget.dismissal?.dismissThresholds[actionType] ?? _kDismissThreshold;
+  double get _dismissThreshold {
+    if (widget.dismissal == null) {
+      return _kDismissThreshold;
+    }
+    return widget.dismissal?.dismissThresholds[actionType] ??
+        _kDismissThreshold;
+  }
 
   bool get _dismissible => widget.dismissal != null && _dismissThreshold < 1.0;
 

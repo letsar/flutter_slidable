871d34ef08c22479f878b98c84fef7f68611fa7d
saigopin <85918810+saigopin@users.noreply.github.com>
2022-07-13 10:21:46 +0530

    updated to hardEdge

--- lib/src/dismissal.dart
+++ lib/src/dismissal.dart
@@ -144,7 +144,8 @@ class _SizeTransition extends AnimatedWidget {
       alignment = const AlignmentDirectional(0, -1);
     }
     return ClipRect(
-      clipBehavior: value == 1 ? Clip.none : Clip.hardEdge,
+//       clipBehavior: value == 1 ? Clip.none : Clip.hardEdge,
+            clipBehavior: Clip.hardEdge,
       child: Align(
         alignment: alignment,
         heightFactor: axis == Axis.vertical ? value : null,
